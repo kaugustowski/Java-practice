62d72a0a1ff59fe69d33e3db2ae8af9e96069071
kaugustowski <53943285+kaugustowski@users.noreply.github.com>
2021-01-22 22:58:13 +0100

    added solution for leetcode 771
    Jewels and Stones

---
+++ src/test/java/leetcode/JewelsAndStonesTest.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JewelsAndStonesTest {
+    JewelsAndStones jewelsAndStones;
+
+    @BeforeEach
+    void setUp() {
+        jewelsAndStones = new JewelsAndStones();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInput")
+    void solution(String jewels, String stones, int expected){
+        int actual = jewelsAndStones.numJewelsInStones(jewels, stones);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideInput() {
+        return Stream.of(
+                Arguments.of("aA", "aAAbbbb", 3),
+                Arguments.of("z", "ZZ", 0),
+                Arguments.of("abcDEF", "aabbbaaccDDDdddeeeEffFzxcZXC", 15)
+        );
+    }
+}
