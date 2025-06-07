3e2ae000a9bc5032e88f90dd892589375eb57089
kaugustowski <53943285+kaugustowski@users.noreply.github.com>
2025-06-07 22:44:37 +0200

    add leetcode solutions for Best time to buy and Sell Stock 1 and 2

---
+++ src/test/java/leetcode/BestTimeToBuyAndSellStock2Test.java
@@ -0,0 +1,31 @@
+import leetcode.BestTimeToBuyAndSellStock2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BestTimeToBuyAndSellStock2Test {
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    public void testMaxProfit(int[] prices, int expected) {
+        BestTimeToBuyAndSellStock2 bestTimeToBuyAndSellStock2 = new BestTimeToBuyAndSellStock2();
+        assertEquals(expected, bestTimeToBuyAndSellStock2.maxProfit(prices));
+    }
+
+    private static Arguments[] provideTestCases() {
+        return new Arguments[] {
+            Arguments.of(new int[] {7, 1, 5, 3, 6, 4}, 7),
+            Arguments.of(new int[] {1, 2, 3, 4, 5}, 4),
+            Arguments.of(new int[] {7, 6, 4, 3, 1}, 0),
+            Arguments.of(new int[] {1, 5, 3, 7, 2, 8}, 14),
+            Arguments.of(new int[] {10, 22, 5, 75, 65, 80}, 97),
+            Arguments.of(new int[] {100, 180, 260, 310, 40, 535, 695}, 1095),
+            Arguments.of(new int[] {}, 0),
+            Arguments.of(new int[] {1}, 0),
+            Arguments.of(new int[] {1, 1}, 0)
+        };
+    }
+}
